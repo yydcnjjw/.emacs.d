70a7a84ef36d1fabb00a7afe11d79d1af2b75b98
yydcnjjw <yydcnjjw@gmail.com>
2018-10-13 14:26:31 +0800

    fix: add condition dot

--- lisp/lang/conf-dot.el
+++ lisp/lang/conf-dot.el
@@ -1,8 +1,9 @@
-(require-package 'graphviz-dot-mode)
-(with-eval-after-load 'graphviz-dot-mode
-  (setq graphviz-dot-auto-indent-on-newline t
-        graphviz-dot-auto-indent-on-braces t
-        graphviz-dot-auto-indent-on-semi t
-        graphviz-dot-indent-width 2))
+(when (executable-find "dot")
+  (require-package 'graphviz-dot-mode)
+  (with-eval-after-load 'graphviz-dot-mode
+    (setq graphviz-dot-auto-indent-on-newline t
+          graphviz-dot-auto-indent-on-braces t
+          graphviz-dot-auto-indent-on-semi t
+          graphviz-dot-indent-width 2)))
 
 (provide 'conf-dot)
