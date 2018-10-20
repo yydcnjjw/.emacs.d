31986bb716b3984051257b979baf355e52f0b3d6
yydcnjjw <yydcnjjw@gmail.com>
2018-10-20 15:21:17 +0800

    fix: readjust dir structure

--- lisp/project-mode/conf-qt-project.el
+++ lisp/project-mode/conf-qt-project.el
@@ -2,7 +2,24 @@
   "Check if a project contains qt pro files."
   (projectile-verify-file-wildcard "*.pro"))
 
-(projectile-register-project-type 'qt-project #'projectile-qt-project-p)
+(defvar-local qt/build-type "Debug")
+(defvar-local qt/build-option "-spec linux-clang")
+(defvar-local qt/build-dir "debug")
+(defvar-local qt/build-file nil)
+
+(defun my/qt-configure-cmd()
+  (if qt/build-file
+      (format "cd %s;qmake ../%s %s" qt/build-dir qt/build-file qt/build-option)
+    (message "require build file")))
+(defun my/qt-compilation-cmd()
+  (format "cd %s;bear make" qt/build-dir))
+(defun my/qt-run-cmd()
+  qt/build-dir)
+
+(projectile-register-project-type 'qt-project #'projectile-qt-project-p
+                                  :configure 'my/qt-configure-cmd
+                                  :compile 'my/qt-compilation-cmd
+                                  :run 'my/qt-run-cmd)
 
 (skeletor-define-template "qt-project"
   :title "qt-project"
@@ -10,7 +27,7 @@
   :after-creation
   (lambda (dir)
     (skeletor-async-shell-command (format
-				   "ln -s %s/build/Debug/compile_commands.json %s/compile_commands.json" dir dir))))
+				   "ln -s %s/debug/compile_commands.json %s/compile_commands.json" dir dir))))
 
 
 (provide 'conf-qt-project)
