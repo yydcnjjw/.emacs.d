415eba6c6cc738b1a51a4cdee6d2b557e9e594d8
yydcnjjw <yydcnjjw@gmail.com>
2018-12-06 14:05:29 +0800

    feat: haskell support

---
+++ lisp/lang/conf-haskell.el
@@ -0,0 +1,9 @@
+(require-package 'haskell-mode)
+(when (executable-find "stack")
+  (require-package 'intero)
+  (add-hook 'haskell-mode-hook
+            (lambda ()
+              (intero-mode 1)
+              )))
+
+(provide 'conf-haskell)
