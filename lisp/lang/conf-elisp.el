0f857f46d345b11824d5da2d5776c8b18fc0fcf5
yydcnjjw <yydcnjjw@gmail.com>
2018-12-13 11:36:15 +0800

    fix: add paredit-mode for elisp

--- lisp/lang/conf-elisp.el
+++ lisp/lang/conf-elisp.el
@@ -1,4 +1,9 @@
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (my/local-push-company-backend '(company-elisp company-yasnippet))))
+(defun load-elisp-conf ()
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda ()
+              (my/local-push-company-backend
+               '(company-elisp company-yasnippet))
+              (paredit-mode 1))))
+(add-hook 'after-init-hook 'load-elisp-conf)
+
 (provide 'conf-elisp)
