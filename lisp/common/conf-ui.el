a8eb0ddeb0dda2454010453f4bc6ef6caefb054a
yydcnjjw <yydcnjjw@gmail.com>
2019-02-01 12:41:53 +0800

    refactor: ui configure

--- lisp/common/conf-ui.el
+++ lisp/common/conf-ui.el
@@ -12,14 +12,13 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
 (setq inhibit-splash-screen 1
       ring-bell-function 'ignore)
 
 (when window-system
   (setq-default cursor-type 'bar)
-  (column-number-mode 1)
-  (setq
-   frame-resize-pixelwise t))
+  (setq frame-resize-pixelwise t))
 
 (require-package 'spacemacs-theme)
 (load-theme 'spacemacs-dark t)
@@ -28,8 +27,8 @@
 (setq display-line-numbers-width-start t)
 (set-face-attribute 'line-number nil
                     :background nil)
-
 (my/add-hook-prog-and-text-mode 'display-line-numbers-mode)
+(column-number-mode 1)
 (global-hl-line-mode 1)
 
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
