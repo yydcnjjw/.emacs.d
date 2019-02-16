15dbf864163a05c6f82327bb426cef0535ad7a0f
yydcnjjw <yydcnjjw@gmail.com>
2019-02-16 19:54:28 +0800

    fix: support web-mode complete

--- lisp/lang/conf-web.el
+++ lisp/lang/conf-web.el
@@ -12,14 +12,10 @@
 (setq web-mode-markup-indent-offset 2
       web-mode-css-indent-offset 2
       web-mode-code-indent-offset 2
-
       web-mode-enable-current-column-highlight t
       web-mode-enable-current-element-highlight t
-      
       web-mode-enable-auto-pairing t
       web-mode-enable-css-colorization t
-      web-mode-enable-block-face t
-      web-mode-enable-part-face t
       web-mode-enable-comment-interpolation t)
 
 (require-package 'company-web)
@@ -28,9 +24,14 @@
   (let ((file-extension (file-name-extension buffer-file-name)))
     (when (or (string= "html" file-extension)
 	      (string= "css" file-extension))
-      (my/local-push-company-backend '(company-css company-web-html company-yasnippet company-files)))
-    ))
+      (flycheck-mode 1)
+      (my/local-push-company-backend '(company-css company-web-html company-yasnippet)))))
 (add-hook 'web-mode-hook #'my/company-web-configure)
+
+(defun my/lsp-web-enable ()
+  (setq company-lsp-cache-candidates t)
+  (my/lsp-enable))
+;; (add-hook 'web-mode-hook #'my/lsp-web-enable)
 
 ;;; `vue-mode'
 (require-package 'vue-mode)
@@ -39,8 +40,9 @@
   (my/lsp-enable))
 (add-hook 'vue-mode-hook #'my/lsp-vue-enable)
 
-;; js ts tsx jsx
+;; js ts jsx
 (require-package 'tide)
+(require-package 'rjsx-mode)
 (require-package 'flycheck-pos-tip)
 (defun setup-tide-mode ()
   (interactive)
@@ -50,32 +52,37 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  (local-set-key (kbd "M-RET") #'tide-fix)
-  (when (eq major-mode 'js2-mode)
-    (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)))
+  (local-set-key (kbd "M-RET") #'tide-fix))
 (setq tide-completion-detailed t
       tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(setq js-indent-level 8)
+
+(add-hook 'js-mode-hook
+	  #'(lambda ()
+	      (when (eq major-mode 'js-mode)
+		(js2-mode))))
 (add-hook 'js2-mode-hook #'setup-tide-mode)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-hook 'rjsx-mode-hook
+	  #'(lambda ()
+	      (setup-tide-mode)
+	      (yas-activate-extra-mode 'js-mode)))
 (add-hook 'web-mode-hook
           #'(lambda ()
 	      (let ((file-extension (file-name-extension buffer-file-name)))
-		(cond
-		 ((string-equal "tsx" file-extension)
+		(when (string-equal "tsx" file-extension)
 		  (setup-tide-mode)
-		  (flycheck-add-mode 'typescript-tslint 'web-mode))
-		 ((string-equal "jsx" file-extension)
-		  (setup-tide-mode)
-		  (flycheck-add-mode 'javascript-eslint 'web-mode)
-		  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-		  )))))
+		  (yas-activate-extra-mode 'typescript-mode)))))
+(with-eval-after-load 'flycheck
+  (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append))
 
 
-;FIXME: [tsserver] /usr/bin/npm not found
+;;; FIXME: [tsserver] /usr/bin/npm not found
 ;;; ln -s /usr/bin/npm npm
 ;; (require-package 'typescript-mode)
 ;; (defun my/lsp-js-enable ()
@@ -91,10 +98,10 @@
 (add-hook 'sgml-mode-hook #'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  #'emmet-mode) ;; enable Emmet's css abbreviation
 (add-hook 'web-mode-before-auto-complete-hooks
-    #'(lambda ()
-	(let ((web-mode-cur-language
-  	       (web-mode-language-at-pos)))
-          (if (string= web-mode-cur-language "css")
-    	      (setq emmet-use-css-transform t)
-      	    (setq emmet-use-css-transform nil)))))
+	  #'(lambda ()
+	      (let ((web-mode-cur-language
+  		     (web-mode-language-at-pos)))
+		(if (string= web-mode-cur-language "css")
+    		    (setq emmet-use-css-transform t)
+      		  (setq emmet-use-css-transform nil)))))
 (provide 'conf-web)
