eceeb0debb26e642b33e39623ae6c27beb9e7325
yydcnjjw <yydcnjjw@gmail.com>
2019-02-16 20:13:15 +0800

    remove virtualenv option message

--- lisp/lang/conf-python.el
+++ lisp/lang/conf-python.el
@@ -39,36 +39,33 @@
 (my/set-python-lsp-support)
 (my/set-python-jupyter-support)
 
-(if (executable-find "virtualenv")
-    (progn 
-      (require-package 'virtualenvwrapper)
-      ;; (venv-initialize-eshell)
-      ;; (venv-initialize-interactive-shells)
-      (setq venv-location "~/resources/mathematics/venv/")
-      (add-hook 'venv-postactivate-hook #'my/set-python-shell-interpreter)
-      (setq-default mode-line-format (cons '(:exec venv-current-name) mode-line-format))
-      (defvar-local my/venv "python3")
-      (defvar-local my/python-path '(""))
-      (defvar-local my/ld-library-path '(""))
-      (add-hook 'venv-postactivate-hook
-                #'(lambda ()
-                    (dolist (path my/python-path)
-                      (my/append-env-value "PYTHONPATH" path))
-                    (dolist (path my/ld-library-path)
-                      (my/append-env-value "LD_LIBRARY_PATH" path))
-                    (my/set-python-shell-interpreter)
-                    (my/set-python-lsp-support)
-                    (my/set-python-jupyter-support)))
+(when (executable-find "virtualenv")    
+  (require-package 'virtualenvwrapper)
+  ;; (venv-initialize-eshell)
+  ;; (venv-initialize-interactive-shells)
+  (setq venv-location "~/resources/mathematics/venv/")
+  (add-hook 'venv-postactivate-hook #'my/set-python-shell-interpreter)
+  (setq-default mode-line-format (cons '(:exec venv-current-name) mode-line-format))
+  (defvar-local my/venv "python3")
+  (defvar-local my/python-path '(""))
+  (defvar-local my/ld-library-path '(""))
+  (add-hook 'venv-postactivate-hook
+            #'(lambda ()
+                (dolist (path my/python-path)
+                  (my/append-env-value "PYTHONPATH" path))
+                (dolist (path my/ld-library-path)
+                  (my/append-env-value "LD_LIBRARY_PATH" path))
+                (my/set-python-shell-interpreter)
+                (my/set-python-lsp-support)
+                (my/set-python-jupyter-support)))
 
-      (add-hook 'venv-postdeactivate-hook
-                #'(lambda ()
-                    (setenv "PYTHONPATH" "")
-                    (setenv "LD_LIBRARY_PATH" "")))
-      (add-hook 'python-mode-local-vars-hook
-                #'(lambda ()
-                    (when my/venv
-                      (venv-workon my/venv)))))
-  (progn
-    (message "option: require virtualenv")))
+  (add-hook 'venv-postdeactivate-hook
+            #'(lambda ()
+                (setenv "PYTHONPATH" "")
+                (setenv "LD_LIBRARY_PATH" "")))
+  (add-hook 'python-mode-local-vars-hook
+            #'(lambda ()
+                (when my/venv
+                  (venv-workon my/venv)))))
 
 (provide 'conf-python)
